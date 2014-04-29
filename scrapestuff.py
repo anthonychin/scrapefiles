e9385f27501ad03fd5d8f2a4b1a89601f7d4a709
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2014-04-29 03:07:31 -0400

    can now scrape to a new folder

--- scrapestuff.py
+++ scrapestuff.py
@@ -1,7 +1,6 @@
 import cookielib
 import mechanize
 import os
-#import re
 import sys
 import urllib
 import urllib2
@@ -9,29 +8,36 @@ from bs4 import BeautifulSoup
 from os.path import basename
 from os.path import splitext
 from urlparse import urlsplit
-# prog = re.compile(pattern)
 
 #http://www.cim.mcgill.ca/~langer/251.html
 def prompt():
-  print "hello world"
-  user_url = raw_input("Enter url: ").lower()
-  #result = re.match(pattern, url)
-  #print result
+  user_url = raw_input("Enter url to scrape: ").lower()
   url = configure_url(user_url)
-  print url
+  # print url
   page = urllib2.urlopen(url).read()
   soup = BeautifulSoup(page)
-  print soup.prettify() #made the html nice
-
+  # print soup.prettify() #made the html nice
+  folder_name = raw_input("Enter folder name to copy file: ")
+  create_folder(folder_name)
+  # urllib.urlretrieve(url, os.path.join(folder_name, filename))
   for f in soup.find_all('a'):
-    print f.get('href')
-    get_files(f.get('href'))
-
+    get_files(f.get('href'), folder_name)
+  print 'Success, files have been saved'
   #soup.body.p.b finds the first bold item inside a paragraph tag inside a body  
 
-def get_files(url):
-  #print url.split('/')
-  urllib.urlretrieve(url, url.split('/')[-1].split('#')[0].split('?')[0])
+def get_files(url, folder_name):
+  print url
+  filename = url.split('/')[-1]
+  if filename:
+    fullfilename = os.path.join(folder_name, filename)
+    urllib.urlretrieve(url, fullfilename)
+  # print fullfilename
+  # print fullfilename
+  # if (os.path.exists('./comp251langer')):
+  #   print 'this exist'
+  # else:
+  #   print 'this doesnt'
+  #urllib.urlretrieve(url, url.split('/')[-1].split('#')[0].split('?')[0])
 
   # print get_filename_parts_from_url(url)
   #urllib.urlretrieve(url, get_filename_parts_from_url(url))
@@ -40,15 +46,14 @@ def get_files(url):
   # soup = BeautifulSoup(page)
   # print soup.prettify()
 
-def get_filename_parts_from_url(url):
-    fullname = url.split('/')[-1].split('#')[0].split('?')[0]
-    t = list(os.path.splitext(fullname))
-    if t[1]:
-        t[1] = t[1][1:]
-    return t
+
+def create_folder(folder):
+  if not os.path.exists(folder):
+    os.makedirs(folder)
+  print folder
 
 def configure_url(url):
-  if (not (url.startswith('http://') or url.startswith('https://'))):
+  if not (url.startswith('http://') or url.startswith('https://')):
     print "Cannot find http or https protocol, adding http"
     url = 'http://' + url
   return url
